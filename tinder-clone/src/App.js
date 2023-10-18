16180fd2029d45c85215ab9b96fa3e940324999a
adithyaabinoy <adithyaabinoy@gmail.com>
2023-10-18 21:13:25 +0530

    Intergrated Material-UI icons for UI components

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import './App.css';
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
     <h1>Tinder clone</h1>
+    {/* header */}
+    < Header />
+    {/*  tinder cards */}
+    {/*  buttons below tinder cards */}
+
+    {/* chatScreen */}
+    {/* individual chat screen */}
+
+
     </div>
   );
 }
