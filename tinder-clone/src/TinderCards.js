feca1812dccc60cb7b8c1f5d7850aa25d071cc91
adithyaabinoy <adithyaabinoy@gmail.com>
2023-10-19 16:48:17 +0530

    Integrated Firebase Data

--- tinder-clone/src/TinderCards.js
+++ tinder-clone/src/TinderCards.js
@@ -1,22 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import database from './firebase';
 import "./TinderCard.css";
 import TinderCard from "react-tinder-card";
 
 function TinderCards() {
-  const [people, setPeople] = useState([
-    {
-      name: "Ethan dolan",
-      url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8_R9ifNgxEs6IZFAeW85MDPpjZwSbDQv9FDi1NASflHsTv_Ri",
-    },
-    {
-      name: "Grayson Dolan",
-      url: "https://img.wattpad.com/cover/135681082-256-k364639.jpg",
-    },
-  ]);
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    database.collection('people').onSnapshot(snapshot => (
+      setPeople(snapshot.docs.map(doc => doc.data()))
+    ))
+
+  }, []);
 
   return (
     <div>
-      <h1>tinder</h1>
       <div className="tinderCards__cardContainer">
         {people.map((person) => (
           <TinderCard
--- tinder-clone/src/firebase.js
+++ tinder-clone/src/firebase.js
@@ -1,4 +1,6 @@
-import firebase from "firebase";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyBPO1yCgO1U0o4I-RTbAYIxZkdBmakO-zM",
