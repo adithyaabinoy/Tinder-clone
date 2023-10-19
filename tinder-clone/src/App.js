632e38c1a1d0e527c50a287a4cdd2bb7a941f6c9
adithyaabinoy <adithyaabinoy@gmail.com>
2023-10-19 12:04:03 +0530

    Installed React Router DOM

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -1,18 +1,13 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 import Header from "./Header";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-    < Header />
-    {/*  tinder cards */}
-    {/*  buttons below tinder cards */}
-
-    {/* chatScreen */}
-    {/* individual chat screen */}
-
-
+      <Header />
+      
     </div>
   );
 }
