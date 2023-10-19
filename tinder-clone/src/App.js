af2fcc2d243dbe0fcf8d157168a27b356352768a
adithyaabinoy <adithyaabinoy@gmail.com>
2023-10-19 12:14:12 +0530

    Implemented Routing for Navigation

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Router>
+        <Switch>
+        <Route path="/chat">chat</Route>
+          <Route path="/">home</Route>
+        </Switch>
+
+      </Router>
       
     </div>
   );
