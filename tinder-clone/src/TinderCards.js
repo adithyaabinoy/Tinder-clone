2ae08357a4bf686521b92499c6bcbd8eb1647e98
adithyaabinoy <adithyaabinoy@gmail.com>
2023-10-19 15:54:41 +0530

    Added Firebase for Database

--- tinder-clone/src/TinderCards.js
+++ tinder-clone/src/TinderCards.js
@@ -17,20 +17,22 @@ function TinderCards() {
   return (
     <div>
       <h1>tinder</h1>
-
-      {people.map((person) => (
-      <TinderCard
-      className="swipe"
-      key={person.name}
-      preventSwipe={['up', 'down']}
-      >
-        <div
-        style={{ backgroundImage: `url(${person.url})`}}
-        className="card">
-          <h3>{person.name}</h3>
-        </div>
-      </TinderCard>
-      ))}  
+      <div className="tinderCards__cardContainer">
+        {people.map((person) => (
+          <TinderCard
+            className="swipe"
+            key={person.name}
+            preventSwipe={["up", "down"]}
+          >
+            <div
+              style={{ backgroundImage: `url(${person.url})` }}
+              className="card"
+            >
+              <h3>{person.name}</h3>
+            </div>
+          </TinderCard>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ tinder-clone/src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBPO1yCgO1U0o4I-RTbAYIxZkdBmakO-zM",
+  authDomain: "tinder-clone-306db.firebaseapp.com",
+  projectId: "tinder-clone-306db",
+  storageBucket: "tinder-clone-306db.appspot.com",
+  messagingSenderId: "411029428131",
+  appId: "1:411029428131:web:f4d7385b431019307cec16",
+  measurementId: "G-G8NSCJWXS0",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const database = firebaseApp.firestore();
+
+export default database;
