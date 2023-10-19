201fdcd4ff7b33c573043949efaaf89d26ea4d6d
adithyaabinoy <adithyaabinoy@gmail.com>
2023-10-19 12:20:20 +0530

    Implemented React Tinder Cards for Swiping Functionality

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import TinderCards from "./TinderCards";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <Router>
         <Switch>
         <Route path="/chat">chat</Route>
-          <Route path="/">home</Route>
+          <Route path="/"></Route>
+          <TinderCards />
         </Switch>
 
       </Router>
